24e902befe20b446b5f82f2ca90717e5f3e754ca
realnimish <nimish@azero.domains>
2023-01-03 21:46:30 +0530

    Added tests for the merkle_verifier

--- merkle_verifier/lib.rs
+++ merkle_verifier/lib.rs
@@ -18,6 +18,7 @@ mod merkle_verifier {
             Self { root }
         }
 
+        /// Returns the merkle root
         #[ink(message)]
         pub fn root(&self) -> [u8; 32] {
             self.root
@@ -49,44 +50,81 @@ mod merkle_verifier {
         }
     }
 
-    /// Unit tests in Rust are normally defined within such a `#[cfg(test)]`
-    /// module and test functions are marked with a `#[test]` attribute.
-    /// The below code is technically just normal Rust code.
     #[cfg(test)]
     mod tests {
         /// Imports all the definitions from the outer scope so we can use them here.
         use super::*;
-        use rs_merkle::{algorithms::Sha256, Hasher, MerkleProof, MerkleTree};
+        use ink::env::hash::{CryptoHash, Sha2x256};
 
-        /// We test a simple use case of our contract.
         #[ink::test]
-        fn it_works() {
-            let leaf_values = ["a", "b", "c", "d", "e", "f"];
-            let leaves: Vec<[u8; 32]> = leaf_values
+        fn root_works() {
+            let root = [0xff; 32];
+            let merkle_verifier = MerkleVerifier::new(root);
+
+            assert_eq!(merkle_verifier.root(), root);
+        }
+
+        // Test that the param ordering should not matter
+        #[ink::test]
+        fn compute_hash_works() {
+            let first = [0x00; 32];
+            let second = [0xff; 32];
+
+            assert_eq!(
+                MerkleVerifier::compute_hash(&first, &second),
+                MerkleVerifier::compute_hash(&second, &first)
+            );
+        }
+
+        #[ink::test]
+        fn verify_proof_works() {
+            /*
+                Tree structure used for tesing
+
+                              H(ABCD)               <----- Root (Hash: Keccak256)
+                          /            \
+                       H(AB)          H(CD)         <----- Internal Nodes (Hash: Keccak256)
+                      /     \         /    \
+                    H(A)    H(B)    H(C)    H(D)    <----- Leaves (Hash: SHA2)
+                     |       |       |       |
+                     A      *B       C       D      <----- Items
+            */
+
+            // Items for which Merkle tree is to be constructed
+            let items = ["a", "b", "c", "d"];
+
+            // Leaves are the hash (used SHA2 here) of items
+            let leaves: Vec<[u8; 32]> = items
                 .iter()
-                .map(|x| Sha256::hash(x.as_bytes()))
+                .map(|x| {
+                    let mut output = [0u8; 32];
+                    Sha2x256::hash(x.as_bytes(), &mut output);
+                    output
+                })
                 .collect();
 
-            let merkle_tree = MerkleTree::<Sha256>::from_leaves(&leaves);
-            let indices_to_prove = vec![3, 4];
-            let leaves_to_prove = leaves.get(3..5).ok_or("can't get leaves to prove").unwrap();
-            let merkle_proof = merkle_tree.proof(&indices_to_prove);
-            let merkle_root = merkle_tree
-                .root()
-                .ok_or("couldn't get the merkle root")
-                .unwrap();
-            // Serialize proof to pass it to the client
-            let proof_bytes = merkle_proof.to_bytes();
+            let internal_nodes = [
+                MerkleVerifier::compute_hash(&leaves[0], &leaves[1]),
+                MerkleVerifier::compute_hash(&leaves[2], &leaves[3]),
+            ];
 
-            // Parse proof back on the client
-            let proof = MerkleProof::<Sha256>::try_from(proof_bytes).unwrap();
+            let root = MerkleVerifier::compute_hash(&internal_nodes[0], &internal_nodes[1]);
 
-            assert!(proof.verify(
-                merkle_root,
-                &indices_to_prove,
-                leaves_to_prove,
-                leaves.len()
-            ));
+            // Create the MerkleVerifier contract
+            let merkle_verifier = MerkleVerifier::new(root);
+
+            // Prove leaves[1] is a part of the tree
+            let leaf = leaves[1];
+
+            // Case 1: Construct an invalid proof
+            let proof = vec![leaves[0], internal_nodes[0]];
+            let res = merkle_verifier.verify_proof(leaf, proof);
+            assert_eq!(res, false);
+
+            // Case 2: Construct a valid proof
+            let proof = vec![leaves[0], internal_nodes[1]];
+            let res = merkle_verifier.verify_proof(leaf, proof);
+            assert_eq!(res, true);
         }
     }
 }
