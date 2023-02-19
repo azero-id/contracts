d1ba6c0bba08108fcdd0f20eb29764ab1140cf1f
Dennis Zoma <dennis@azero.domains>
2023-02-19 15:10:23 +0100

    feat: Improve build-all & test-all bash scripts

--- build-all.sh
+++ build-all.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash -eu
 
-cargo +stable contract build --optimization-passes=0 --release --manifest-path azns_name_checker/Cargo.toml
-cargo +stable contract build --optimization-passes=0 --release --manifest-path azns_registry/Cargo.toml
+contracts=( "azns_name_checker" "azns_fee_calculator" "merkle_verifier" "azns_registry" )
 
-mkdir -p target
-cp azns_name_checker/target/ink/azns_name_checker.contract target/
-cp azns_registry/target/ink/azns_registry.contract target/
+for i in "${contracts[@]}"
+do
+  echo -e "\nBuilding './$i/Cargo.toml'…"
+  cargo contract build --release --quiet --manifest-path $i/Cargo.toml
+
+  echo "Copying build files to './deployments/$i/'…"
+  mkdir -p ./deployments/$i
+  cp ./target/ink/$i/$i.contract ./deployments/$i/
+  cp ./target/ink/$i/$i.wasm ./deployments/$i/
+  cp ./target/ink/$i/$i.json ./deployments/$i/metadata.json
+done
